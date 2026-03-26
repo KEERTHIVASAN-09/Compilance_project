fa2cdc18e8e73ffba39d545c704b2119046907e8
Vasan <vasansellakannu@gmail.com>
2026-03-26 14:33:49 +0530

    ll

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// ✅ Your backend URL
-export const API_BASE =
-  import.meta.env.VITE_API_URL || "https://compilance-system-project.onrender.com";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
